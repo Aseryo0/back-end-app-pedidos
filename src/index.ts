65a6721cafc029ae9a34fb09a809102640d9057f
Aseryo0 <andrefelipeil@hotmail.com>
2023-11-09 17:17:19 -0300

    criado endpoint snacks e aidionado seeds ao db

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,39 @@
-import dotenv from "dotenv"
-import express, { Express, Request, Response } from "express"
+import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
 
-dotenv.config()
+dotenv.config();
 
-const app: Express = express()
-const port = process.env.PORT || 5000
+const app: Express = express();
+const port = process.env.PORT || 5000;
+const prisma = new PrismaClient();
 
-app.use(express.json())
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
-  const { message } = req.body
+  const { message } = req.body;
 
-  if (!message) return res.status(400).send({ error: "Message is required" })
+  if (!message) return res.status(400).send({ error: "Message is required" });
 
-  res.send({ message })
-})
+  res.send({ message });
+});
+
+app.get("/snacks", async (req: Request, res: Response) => {
+  const { snack } = req.query;
+
+  if (!snack) return res.status(400).send({ error: "Snack is required" });
+
+  const snacks = await prisma.snack.findMany({
+    where: {
+      snack: {
+        equals: snack as string,
+      },
+    },
+  });
+
+  res.send(snacks);
+});
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+  console.log(`Server running on port ${port}`);
+});
